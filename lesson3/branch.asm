850c2ac6d9bcb36dc536504855acaaaf0d157aba
Rob Prouse <rob@prouse.org>
2021-01-24 20:50:54 -0500

    Finish lesson 4

---
+++ lesson3/branch.asm
@@ -0,0 +1,71 @@
+.org $080D
+.segment "STARTUP"
+.segment "INIT"
+.segment "ONCE"
+.segment "CODE"
+
+CHROUT = $FFD2
+NEWLINE = $0D
+
+  jmp start
+
+data:
+  .byte $01,$23,$45,$67,$89,$AB,$CD,$EF
+
+start:
+  ; Branching if less than (N set)
+  ldx #0
+
+n_loop:
+  lda data,x
+  cmp #$55
+  bmi next_n
+  txa
+  ora #$30
+  jsr CHROUT
+
+next_n:
+  inx
+  cpx #8
+  bne n_loop
+  lda #NEWLINE
+  jsr CHROUT
+
+  ; Branching if carry (C set)
+  ldx #0
+
+c_loop:
+  lda data,x
+  clc
+  adc #$55
+  bcs next_c
+  txa
+  ora #$30
+  jsr CHROUT
+
+next_c:
+  inx
+  cpx #8
+  bne c_loop
+  lda #NEWLINE
+  jsr CHROUT
+
+  ; Branching if overflow (V set)
+  ldx #0
+
+v_loop:
+  lda data,x
+  clc
+  adc #$55
+  bvs next_v
+  txa
+  ora #$30
+  jsr CHROUT
+
+next_v:
+  inx
+  cpx #8
+  bne v_loop
+  lda #NEWLINE
+  jsr CHROUT
+  rts
---
+++ lesson3/build.ps1
@@ -0,0 +1,1 @@
+&cl65 -t cx16 -o branch.prg -l branch.list branch.asm
